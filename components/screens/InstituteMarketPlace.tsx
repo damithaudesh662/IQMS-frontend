70a90530758254207bd0d3b79afa756e26511e62
Indeera Udayanga <129371871+Indeera01@users.noreply.github.com>
2025-05-11 00:35:30 +0530

    created the structure for Institute MP

---
+++ components/screens/InstituteMarketPlace.tsx
@@ -0,0 +1,10 @@
+import { Text } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+export default function InstituteMarketPlace() {
+  return (
+    <SafeAreaView>
+      <Text>Institutes</Text>
+    </SafeAreaView>
+  );
+}
