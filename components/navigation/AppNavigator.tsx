62d973e21aaf00be395d373706225c6cdbc16b90
damithaudesh662 <damithavidanapathirana2000@gmail.com>
2025-05-10 13:45:12 +0530

    added create user and admin pages to navigation

--- components/navigation/AppNavigator.tsx
+++ components/navigation/AppNavigator.tsx
@@ -1,13 +1,16 @@
-import React from 'react';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import React from "react";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
-import HomeScreen from '../screens/HomeScreen';
-import SignInScreen from '../screens/SignInScreen';
-
+import HomeScreen from "../screens/HomeScreen";
+import SignInScreen from "../screens/SignInScreen";
+import CreateUserAccount from "../screens/CreateUserAccount"; // Import your CreateUserAccount screen
+import CreateAdminAccount from "../screens/CreateAdminAccount"; // Import your CreateAdminAccount screen
 // Define the types for our navigation stack
 export type RootStackParamList = {
   Home: undefined;
   SignIn: undefined;
+  CreateUserAccount: undefined;
+  CreateAdminAccount: undefined; // Add the CreateUserAccount screen type
 };
 
 const Stack = createNativeStackNavigator<RootStackParamList>();
@@ -18,11 +21,13 @@ const AppNavigator = () => {
       initialRouteName="Home"
       screenOptions={{
         headerShown: false,
-        contentStyle: { backgroundColor: '#f7f7f7' }
+        contentStyle: { backgroundColor: "#f7f7f7" },
       }}
     >
       <Stack.Screen name="Home" component={HomeScreen} />
       <Stack.Screen name="SignIn" component={SignInScreen} />
+      <Stack.Screen name="CreateUserAccount" component={CreateUserAccount} />
+      <Stack.Screen name="CreateAdminAccount" component={CreateAdminAccount} />
     </Stack.Navigator>
   );
 };
