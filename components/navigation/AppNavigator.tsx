cc3b6b8b1e6305893bb66b34bff3b1e99b3c3cc2
damithaudesh662 <damithavidanapathirana2000@gmail.com>
2025-05-10 14:13:34 +0530

    added sample dashboards to app navigator

--- components/navigation/AppNavigator.tsx
+++ components/navigation/AppNavigator.tsx
@@ -5,12 +5,17 @@ import HomeScreen from "../screens/HomeScreen";
 import SignInScreen from "../screens/SignInScreen";
 import CreateUserAccount from "../screens/CreateUserAccount"; // Import your CreateUserAccount screen
 import CreateAdminAccount from "../screens/CreateAdminAccount"; // Import your CreateAdminAccount screen
+import AdminDashboard from "../screens/AdminDashboard"; // Import your AdminDashboard screen
+import UserDashboard from "../screens/UserDashboard"; // Import your UserDashboard screen
 // Define the types for our navigation stack
 export type RootStackParamList = {
   Home: undefined;
   SignIn: undefined;
   CreateUserAccount: undefined;
-  CreateAdminAccount: undefined; // Add the CreateUserAccount screen type
+  CreateAdminAccount: undefined; 
+  UserDashboard: undefined; // Add the UserDashboard screen type
+  AdminDashboard: undefined; // Add the AdminDashboard screen type
+  // Add other screens here as needed
 };
 
 const Stack = createNativeStackNavigator<RootStackParamList>();
@@ -28,6 +33,8 @@ const AppNavigator = () => {
       <Stack.Screen name="SignIn" component={SignInScreen} />
       <Stack.Screen name="CreateUserAccount" component={CreateUserAccount} />
       <Stack.Screen name="CreateAdminAccount" component={CreateAdminAccount} />
+      <Stack.Screen name="AdminDashboard" component={AdminDashboard} /> 
+      <Stack.Screen name="UserDashboard" component={UserDashboard} />
     </Stack.Navigator>
   );
 };
